a2c9343b7deed577618eab9d58e5f59571a1534d
Anna Balsanko <annbalsanko@gmail.com>
2022-10-02 16:34:21 +0300

    Code refactoring

--- Fibonnacci_number.cs
+++ Fibonnacci_number.cs
@@ -3,7 +3,11 @@ public static class LoopTasks
 {
     public static int SumOfFirstNFibonacciNumbers(int n)
     {
-        int CurrentValue = 1, PrevValue = 0, Sum = 1, CurrentSum, i = 2;
+        int CurrentValue = 1;
+        int PrevValue = 0;
+        int Sum = 1;
+        int CurrentSum;
+        int i = 2;
 
         if (n == 0) Sum = 0;
         if (n == 1) Sum = 0;
